37a318474c22e53f482961318817d335a4ed7513
Alan Russell <ajrussellaudio@gmail.com>
2017-11-24 22:02:44 +0000

    steer with noise

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
-var stars;
+var stars, xoff = 0.0, yoff = 0.0;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  var numStars = 1200;
+  var numStars = 800;
   stars = new Array(numStars).fill(0).map(function (n) {
     return new Star();
   });
@@ -10,7 +10,9 @@ function setup() {
 
 function draw() {
   background(0);
-  translate(width/2, height/2);
+  xoff += 0.003;
+  yoff += 0.002;
+  translate(noise(xoff) * width, noise(yoff) * height);
   stars.forEach(function (star) {
     star.update();
     star.display();
